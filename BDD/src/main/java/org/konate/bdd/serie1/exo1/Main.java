4ad38acd83b6463f29846fe4118d485739126964
Admin <Admin@PC-ASUS.mshome.net>
2018-11-29 21:17:04 +0100

    Updated Main

--- BDD/src/main/java/org/konate/bdd/serie1/exo1/Main.java
+++ BDD/src/main/java/org/konate/bdd/serie1/exo1/Main.java
@@ -3,6 +3,7 @@ package org.konate.bdd.serie1.exo1;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,12 +17,24 @@ public class Main {
 		String pwd = "Massare@";
 		// TODO Auto-generated method stub
 		Connection connection = DriverManager.getConnection(uri, username, pwd);
-		Statement statement = connection.createStatement();
+		//Statement statement = connection.createStatement();
 		List<Eleve> eleves = Readers.readEleve("./files/eleves.txt");
-		//System.out.println(eleves);
 		
-		/*String query = "";
-		ResultSet resultSet = statement.executeQuery(query );
+		String query;
+		//String EleveCreationQuery = "DROP TABLE IF EXISTS `eleves`; CREATE TABLE `eleves` (`id` int(11) NOT NULL AUTO_INCREMENT, `nom` varchar(30) NOT NULL,`age` int(11) NOT NULL, PRIMARY KEY (`id`), UNIQUE KEY `nom` (`nom`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+		/*String EleveCreationQuery = "CREATE TABLE IF NOT EXISTS Eleve(id SMALLINT  NOT NULL AUTO_INCREMENT PRIMARY KEY"
+				+ ",nom  VARCHAR(30), age int); ";*/
+		String EleveCreationQuery = "CREATE DATABASE IF NOT EXISTS SCHOOL;";
+		PreparedStatement preparedStatement  =  connection.prepareStatement(EleveCreationQuery);
+		preparedStatement.execute();
+		
+		/*ResultSet resultSet = null;
+		
+		for (Eleve eleve : eleves) {
+			query = "INSERT INTO eleves (nom, age) VALUES ('" + eleve.getNom() + "', '" + eleve.getAge() + "')";
+			preparedStatement.execute(query);
+		}*/
+		/*resultSet = statement.executeQuery("SELECT * from `eleves`");
 		while(resultSet.next()) {
 			
 		}*/
