680ceb20140744f33b975b0de0ca602448f01b64
Mousse <moussakonate27@gmail.com>
2018-11-30 22:19:44 +0100

    Updated Main to test all insertions succeded

--- BDD/src/main/java/org/konate/bdd/serie1/exo1/Main.java
+++ BDD/src/main/java/org/konate/bdd/serie1/exo1/Main.java
@@ -1,17 +1,17 @@
 package org.konate.bdd.serie1.exo1;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) throws SQLException, IOException {
+		
 		String uri = "jdbc:mysql://localhost:3306/school";
 		String username = "root";
 		String pwd = "Massare@";
@@ -21,42 +21,23 @@ public class Main {
 		List<Eleve> eleves = Readers.readEleve("./files/eleves.txt");
 		List<Instruments> instruments = Readers.readInstruments("./files/instruments.txt");
 		List<Professeur> profs = Readers.readProfesseurs("./files/professeurs.txt");
-	
-		String createDB = "CREATE DATABASE IF NOT EXISTS school;";
-		String query;
-		String elevesCreationQuery = "USE SCHOOL;" + 
-				"DROP TABLE eleves;" + 
-				"CREATE TABLE eleves (id int(11) NOT NULL AUTO_INCREMENT, nom varchar(30) NOT NULL, age int(11) NOT NULL, PRIMARY KEY (id), UNIQUE KEY nom (nom));";
 		
-		String instrumentsCreationQuery = "DROP TABLE instruments;" + 
-				"CREATE TABLE instruments (id int(11) NOT NULL AUTO_INCREMENT, nom varchar(30) NOT NULL, prix int(11) NOT NULL, prix_cours int(11) NOT NULL, location char(1) NOT NULL, PRIMARY KEY (id), UNIQUE KEY nom (nom));";
-		
-		String professeursCreationQuery = "DROP TABLE professeurs;\r\n" + 
-				"CREATE TABLE professeurs (id int(11) NOT NULL AUTO_INCREMENT, nom varchar(30) NOT NULL, age int(11) NOT NULL, instrument int(11) NOT NULL, PRIMARY KEY (id), UNIQUE KEY nom (nom));";
-		
-		
-		PreparedStatement preparedStatement  =  connection.prepareStatement(createDB);
+		String useDB = "USE school;";
+		PreparedStatement preparedStatement = connection.prepareStatement(useDB);
 		preparedStatement.execute();
 		
-		/*preparedStatement  =  connection.prepareStatement(elevesCreationQuery);
-		preparedStatement.executeQuery();
+		//Read SQL Database and tables creation queries
+		String SQLFile = "./files/musique-creation.sql";
+		FileInputStream fis = new FileInputStream(SQLFile);
+		Readers.readAndExecuteSQLQueries(connection, fis);
 		
-		preparedStatement  =  connection.prepareStatement(professeursCreationQuery);
-		preparedStatement.executeQuery();
-		
-		preparedStatement  =  connection.prepareStatement(instrumentsCreationQuery);
-		preparedStatement.executeQuery();*/
-		
-		
-		ResultSet resultSet = null;
-		
+		String query;
 		for (Eleve eleve : eleves) {
 			
 			query = "INSERT INTO eleves (nom, age) VALUES (?, ?)";
 			preparedStatement  =  connection.prepareStatement(query);
 			preparedStatement.setString(1, eleve.getNom());
 			preparedStatement.setInt(2, eleve.getAge());
-			
 			preparedStatement.execute();
 		}
 		
@@ -71,23 +52,25 @@ public class Main {
 		}
 		
 		for ( Instruments instrument : instruments) {
-				query = "INSERT INTO instruments (nom, prix, prix_cours, location) VALUES (?, ?, ?, ?)";
-				preparedStatement  =  connection.prepareStatement(query);
-				preparedStatement.setString(1, instrument.getNom());
-				preparedStatement.setInt(2, instrument.getPrix());
-				preparedStatement.setInt(3, instrument.getPrixCours());
-				preparedStatement.setString(4, instrument.getLocation());
-			
-			
+			query = "INSERT INTO instruments (nom, prix, prix_cours, location) VALUES (?, ?, ?, ?)";
+			preparedStatement  =  connection.prepareStatement(query);
+			preparedStatement.setString(1, instrument.getNom());
+			preparedStatement.setInt(2, instrument.getPrix());
+			preparedStatement.setInt(3, instrument.getPrixCours());
+			preparedStatement.setString(4, instrument.getLocation());
 			preparedStatement.execute();
 		}
 		
+		/*String joinEleveProfs = "";
+		preparedStatement  =  connection.prepareStatement(joinEleveProfs);
+		preparedStatement.execute();*/
+		
+		/*ResultSet resultSet = null;
 		resultSet = preparedStatement.executeQuery("SELECT * from eleves");
-		preparedStatement = connection.prepareStatement("SELECT * from eleves");
-		ResultSetMetaData rsmd = preparedStatement.getMetaData();
+		ResultSetMetaData rsmd = resultSet.getMetaData();;
 		for (int i = 1; i < rsmd.getColumnCount()+1; i++) {
 			System.out.println(rsmd);
-		}
+		}*/
 		
 	}
 
