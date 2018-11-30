66b08a1e418ee7814b0ae8b12038f2f402c07311
Mousse <moussakonate27@gmail.com>
2018-11-30 22:17:38 +0100

    Added DROP TABLE for avoiding sql exception

--- BDD/files/musique-creation.sql
+++ BDD/files/musique-creation.sql
@@ -1,13 +1,18 @@
+CREATE DATABASE IF NOT EXISTS school;
+USE school;
+
+DROP TABLE `eleves`;
 CREATE TABLE `eleves` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(30) NOT NULL,
   `age` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE `instruments`;
 CREATE TABLE `instruments` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(30) NOT NULL,
   `prix` int(11) NOT NULL,
   `prix_cours` int(11) NOT NULL,
@@ -16,8 +21,9 @@ CREATE TABLE `instruments` (
   UNIQUE KEY `nom` (`nom`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE `professeurs`;
 CREATE TABLE `professeurs` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(30) NOT NULL,
   `age` int(11) NOT NULL,
   `instrument` int(11) NOT NULL,
