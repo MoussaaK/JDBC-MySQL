2095894a913a8a8e58ff3e172c2ce316462ed02e
Mousse <moussakonate27@gmail.com>
2018-12-03 05:54:36 +0100

    Updated Main to test Factory Class Queries

---
+++ BDD/src/main/java/org/konate/bdd/serie1/exo2/Main.java
@@ -0,0 +1,39 @@
+package org.konate.bdd.serie1.exo2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		String uri = "jdbc:mysql://localhost:3306/school";
+		String username = "root";
+		String pwd = "Massare@";
+		
+		Connection connection = DriverManager.getConnection(uri, username, pwd);
+		Statement statement = connection.createStatement();
+		
+		String sql = "SELECT nom, age From school.eleves WHERE eleves.age > 25 GROUP BY nom ASC;";
+		ResultSet resultSet = Queries.execute(statement, sql);
+		Queries.printQueryAnswer(resultSet, "nom");
+		
+		sql = "SELECT AVG(age) as average_age_eleves From school.eleves;";
+		resultSet = Queries.execute(statement, sql);
+		Queries.printQueryAnswer(resultSet, "average_age_eleves");
+		
+		sql = "SELECT AVG(age) as average_age_professeurs From school.professeurs;";
+		resultSet = Queries.execute(statement, sql);
+		Queries.printQueryAnswer(resultSet, "average_age_professeurs");
+		
+		sql = "SELECT e.nom, avg(prix) as depenses FROM school.eleves e INNER JOIN school.eleves_instruments ei ON ei.id = e.id INNER JOIN school.instruments i ON i.nom = ei.instrument GROUP BY e.id;";
+		resultSet = Queries.execute(statement, sql);
+		Queries.printQueryAnswer(resultSet, "nom");
+		resultSet = Queries.execute(statement, sql);
+		Queries.printQueryAnswer(resultSet, "depenses");
+		
+		
+	}
+}
